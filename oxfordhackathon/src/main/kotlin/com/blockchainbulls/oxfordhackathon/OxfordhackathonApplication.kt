1bee6222bee91ec4568d889421d5c8d6afb277ee
Joseph Gwara <jgwarix@gmail.com>
2023-03-28 21:26:35 +0200

    Initial Commit

---
+++ oxfordhackathon/src/main/kotlin/com/blockchainbulls/oxfordhackathon/OxfordhackathonApplication.kt
@@ -0,0 +1,11 @@
+package com.blockchainbulls.oxfordhackathon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OxfordhackathonApplication
+
+fun main(args: Array<String>) {
+	runApplication<OxfordhackathonApplication>(*args)
+}
---
+++ oxfordhackathon/src/test/kotlin/com/blockchainbulls/oxfordhackathon/OxfordhackathonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blockchainbulls.oxfordhackathon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OxfordhackathonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
