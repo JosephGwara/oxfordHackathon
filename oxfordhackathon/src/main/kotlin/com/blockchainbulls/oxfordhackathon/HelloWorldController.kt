22bb09d0d20dddcc6d1e5f76b9b6dc725697b92b
Joseph Gwara <jgwarix@gmail.com>
2023-03-28 22:26:16 +0200

    Created example end point

---
+++ oxfordhackathon/src/main/kotlin/com/blockchainbulls/oxfordhackathon/HelloWorldController.kt
@@ -0,0 +1,17 @@
+package com.blockchainbulls.oxfordhackathon
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+
+    @GetMapping
+    fun helloWorld():String {
+        return "Hello this is an ENDPOINT"
+    }
+}
