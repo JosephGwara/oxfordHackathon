24e864319607f033179dfa64485c1eb2aad78681
Batandwa Mgutsi <b.mgutsi@gmail.com>
2023-03-29 08:14:11 +0200

    Adds auth manager

---
+++ oxfordhackathon/src/main/kotlin/com/blockchainbulls/oxfordhackathon/database/Users.kt
@@ -0,0 +1,24 @@
+/** Manages a collection of users. */
+class Users {
+  val users = arrayListOf<User>()
+  var nextUserId = 0
+
+  fun insertUser(user: User): User {
+    val toInsertUser = User(makeNextUserId(), user.username, user.password)
+    users.add(toInsertUser)
+    return toInsertUser
+  }
+
+  fun canLogin(user: User): User {
+    val out =
+        users.firstNotNullOf({
+          { it.takeIf { it.username == user.username && it.password == user.password } }
+        })()
+
+    return out!!
+  }
+
+  fun makeNextUserId(): Int {
+    return nextUserId++
+  }
+}
---
+++ oxfordhackathon/src/main/kotlin/com/blockchainbulls/oxfordhackathon/models/User.kt
@@ -0,0 +1,7 @@
+
+/**
+ * Stores user information.
+ * 
+ * Note that the user password is not hashed.
+ */
+class User(val id: Int, val username: String, val password: String) {}
